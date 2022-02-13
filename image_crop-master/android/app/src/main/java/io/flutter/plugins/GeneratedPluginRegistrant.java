86b32aaa0268380336d989053b123392b023de9a
ufukoztp <90188671+ufukoztp@users.noreply.github.com>
2022-02-13 13:39:11 +0300

    image and fonts added

---
+++ image_crop-master/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import com.lykhonis.imagecrop.ImageCropPlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    ImageCropPlugin.registerWith(registry.registrarFor("com.lykhonis.imagecrop.ImageCropPlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
+  }
+}
---
+++ image_crop-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ozbysr/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ozbysr/Downloads/asd/bijuteri/image_crop-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ozbysr/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.3.2"
+export "FLUTTER_BUILD_NUMBER=0.3.2"
